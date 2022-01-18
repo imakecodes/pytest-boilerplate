76ae0ff6b837f6961b901ffa1041c19980e90693
Michel Wilhelm <michelwilhelm@gmail.com>
2022-01-18 11:18:19 -0300

    test: adding support for hoverfly

--- gists/github.py
+++ gists/github.py
@@ -9,7 +9,7 @@ class GithubApi:
     BASE_URL = "https://api.github.com"
 
     def __init__(self, custom_token=None):
-        token = os.getenv("GH_TOKEN", custom_token)
+        token = os.getenv("GITHUB_TOKEN", custom_token)
 
         if not token:
             raise exceptions.GithubApiTokenNotFound()
@@ -20,4 +20,5 @@ class GithubApi:
         }
 
     def get(self, path, **kwargs):
-        ...
+        url = f"{self.BASE_URL}/{path}"
+        return requests.get(url, headers=self.headers, **kwargs)
